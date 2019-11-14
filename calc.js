ceecda28a6dd5bda1aeca93d64db217913f6e73b
Taylor <32696683+whybein@users.noreply.github.com>
2019-11-14 23:54:55 +0900

    first version

--- calc.js
+++ calc.js
@@ -1,51 +1,20 @@
 function add(char) {
-    var display = document.getElementById('display');
-    display.value = display.value + char;
+  var display = document.getElementById("display");
+  display.value = display.value + char;
 }
 
 function calculate() {
-    var display = document.getElementById('display');
-    var result = eval(display.value);
-    document.getElementById('result').value = result;
+  var display = document.getElementById("display");
+  var result = eval(display.value);
+  document.getElementById("result").value = result;
 }
 
 function reset() {
-    document.getElementById('display').value = '';
-    document.getElementById('result').value = '';
+  document.getElementById("display").value = "";
+  document.getElementById("result").value = "";
 }
 
 function back() {
-    var value = document.getElementById('display').value;
-    document.getElementById('display').value = value.substr(0, value.length - 1);
+  var value = document.getElementById("display").value;
+  document.getElementById("display").value = value.substr(0, value.length - 1);
 }
-
-var numberClicked = false;
-
-function add(char) {
-    if (numberClicked === false && isNaN(char) === false) {
-        document.getElementById('display').value += char;
-    } else {
-        document.getElementById('display').value += char;
-    }
-
-    if (isNaN(char) === true) {
-        numberClicked = false;
-    } else {
-        numberClicked = true;
-    }
-}
-
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-
-
-
-// var commaOnNumber = Array.from(document.querySelectorAll('input'))[0];
-
-// for (var i in commaOnNumber) {
-//     input.addEventListener('input', function() {
-//         this.value = this.value.replace(/[^0-9 \,]/, '');
-//     });
-// }
